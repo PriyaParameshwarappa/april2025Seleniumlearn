e1fcbebc278dce78c14bcf4b328a499783d1a739
unknown <priya.konemane@gmail.com>
2025-04-08 07:58:12 +0530

    first commit for project

---
+++ src/main/java/april2025Selenium_day1/WebDriverBasics.java
@@ -0,0 +1,24 @@
+package april2025Selenium_day1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverBasics {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+		String title=driver.getTitle();
+		System.out.println(title);
+		if(title.equalsIgnoreCase("google")) {
+			System.out.println("Test Case pass ");
+		}else {
+			System.out.println("Test Case Fail");
+		}
+		
+		driver.close();
+
+	}
+
+}
